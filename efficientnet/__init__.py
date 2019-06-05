042983a0fead3cdcaa85e2da820373cffa9b9a54
mattiavarile <32924751+mattiavarile@users.noreply.github.com>
2019-06-05 10:42:35 +0200

    Update __init__.py

--- efficientnet/__init__.py
+++ efficientnet/__init__.py
@@ -1,4 +1,4 @@
 from .model import *
 from .preprocessing import center_crop_and_resize
 from .preprocessing import preprocess_input
-from keras.models import load_model
+from tensorflow.keras.models import load_model
