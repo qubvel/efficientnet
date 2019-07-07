312c5a0e4e6a7d432011a509e22d27de983d933c
Sasha Illarionov <sasha@sdll.space>
2019-07-07 20:41:09 +0300

    feat: license the code

--- efficientnet/layers.py
+++ efficientnet/layers.py
@@ -1,3 +1,17 @@
+# Copyright 2019 The TensorFlow Authors, Pavel Yakubovskiy. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import tensorflow as tf
 import keras.backend as K
 import keras.layers as KL
@@ -5,40 +19,35 @@ from keras.utils.generic_utils import get_custom_objects
 
 
 class Swish(KL.Layer):
-
     def call(self, inputs):
         return tf.nn.swish(inputs)
 
 
 class DropConnect(KL.Layer):
-
-    def __init__(self, drop_connect_rate=0., **kwargs):
+    def __init__(self, drop_connect_rate=0.0, **kwargs):
         super().__init__(**kwargs)
         self.drop_connect_rate = drop_connect_rate
 
     def call(self, inputs, training=None):
-
         def drop_connect():
             keep_prob = 1.0 - self.drop_connect_rate
 
             # Compute drop_connect tensor
             batch_size = tf.shape(inputs)[0]
             random_tensor = keep_prob
-            random_tensor += tf.random_uniform([batch_size, 1, 1, 1], dtype=inputs.dtype)
+            random_tensor += tf.random_uniform(
+                [batch_size, 1, 1, 1], dtype=inputs.dtype
+            )
             binary_tensor = tf.floor(random_tensor)
             output = tf.div(inputs, keep_prob) * binary_tensor
             return output
-
 
         return K.in_train_phase(drop_connect, inputs, training=training)
 
     def get_config(self):
         config = super().get_config()
-        config['drop_connect_rate'] = self.drop_connect_rate
+        config["drop_connect_rate"] = self.drop_connect_rate
         return config
 
 
-get_custom_objects().update({
-    'DropConnect': DropConnect,
-    'Swish': Swish,
-})
+get_custom_objects().update({"DropConnect": DropConnect, "Swish": Swish})
--- efficientnet/preprocessing.py
+++ efficientnet/preprocessing.py
@@ -1,3 +1,17 @@
+# Copyright 2019 The TensorFlow Authors, Pavel Yakubovskiy. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import numpy as np
 from skimage.transform import resize
 
@@ -5,25 +19,29 @@ MEAN_RGB = [0.485 * 255, 0.456 * 255, 0.406 * 255]
 STDDEV_RGB = [0.229 * 255, 0.224 * 255, 0.225 * 255]
 
 MAP_INTERPOLATION_TO_ORDER = {
-    'nearest': 0,
-    'bilinear': 1,
-    'biquadratic': 2,
-    'bicubic': 3,
+    "nearest": 0,
+    "bilinear": 1,
+    "biquadratic": 2,
+    "bicubic": 3,
 }
 
 
-def center_crop_and_resize(image, image_size, crop_padding=32, interpolation='bicubic'):
+def center_crop_and_resize(image, image_size, crop_padding=32, interpolation="bicubic"):
     assert image.ndim in {2, 3}
     assert interpolation in MAP_INTERPOLATION_TO_ORDER.keys()
 
     h, w = image.shape[:2]
 
-    padded_center_crop_size = int((image_size / (image_size + crop_padding)) * min(h, w))
+    padded_center_crop_size = int(
+        (image_size / (image_size + crop_padding)) * min(h, w)
+    )
     offset_height = ((h - padded_center_crop_size) + 1) // 2
     offset_width = ((w - padded_center_crop_size) + 1) // 2
 
-    image_crop = image[offset_height:padded_center_crop_size + offset_height,
-                       offset_width:padded_center_crop_size + offset_width]
+    image_crop = image[
+        offset_height : padded_center_crop_size + offset_height,
+        offset_width : padded_center_crop_size + offset_width,
+    ]
     resized_image = resize(
         image_crop,
         (image_size, image_size),
