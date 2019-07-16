68bd8d726157276c6dc69da13321f7e8328b205b
Pavel Yakubovskiy <qubvel@gmail.com>
2019-07-16 16:30:09 +0300

    Release/v0.0.4 (#35)
    * Update __version__.py
    * Update setup.py

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=("examples", "misc")),
+    packages=find_packages(exclude=("examples", "misc", "scripts")),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
     # entry_points={
