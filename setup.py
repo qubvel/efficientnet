8aa3fa5479ec006195f220287b2d18eb90405f96
Pavel Yakubovskiy <qubvel@gmail.com>
2019-07-16 16:54:14 +0300

    Fix missing comma

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: Apache Software License"
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
