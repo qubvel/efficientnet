d1aee18bd1f2a31f9b9fe20b0ab020e726db8d66
Pavel Yakubovskiy <qubvel@gmail.com>
2019-07-22 15:21:07 +0300

    Add tests (#41)
    * Add models tests
    * Update travis config
    * Create dockerfile

---
+++ docker/Dockerfile
@@ -0,0 +1,10 @@
+FROM tensorflow/tensorflow:1.12.0-py3
+
+# install keras
+RUN pip install \
+    keras==2.2.4 \
+    scikit-image
+
+RUN pip install pytest
+
+WORKDIR /project
---
+++ tests/test_model.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import pytest
+
+import numpy as np
+import efficientnet as efn
+
+from skimage.io import imread
+from keras.models import load_model
+
+PANDA_PATH = 'misc/panda.jpg'
+
+PANDA_ARGS = [
+    (efn.EfficientNetB0, (388, 0.8347934)),
+    (efn.EfficientNetB1, (388, 0.8702488)),
+    (efn.EfficientNetB2, (388, 0.8227086)),
+    (efn.EfficientNetB3, (388, 0.8152614)),
+    (efn.EfficientNetB4, (388, 0.7353228)),
+    (efn.EfficientNetB5, (388, 0.8103732)),
+]
+
+
+def _select_args(args):
+    is_travis = os.environ.get('TRAVIS', False)
+    if is_travis:
+        return args[:1]
+    else:
+        return args
+
+
+def _get_dummy_input(input_shape):
+    input_shape = [x if x else 1 for x in input_shape]
+    return np.ones(input_shape)
+
+
+def _get_panda_input(input_shape):
+    image = imread(PANDA_PATH)
+    image = efn.center_crop_and_resize(image, input_shape[1])
+    image = efn.preprocess_input(image)
+    image = np.expand_dims(image, 0)
+    return image
+
+
+def test_model_predict():
+    model = efn.EfficientNetB0()
+    input_ = _get_dummy_input(model.input_shape)
+    result = model.predict(input_)
+
+
+def test_model_save_load():
+    model = efn.EfficientNetB0()
+    model.save('/tmp/model.h5')
+    new_model = load_model('/tmp/model.h5')
+
+
+@pytest.mark.parametrize('args', _select_args(PANDA_ARGS))
+def test_models_result(args):
+    model_builder, result = args
+    model = model_builder(weights='imagenet')
+    input_ = _get_panda_input(model.input_shape)
+    prediction = model.predict(input_)
+    assert result[0] == prediction.argmax()
+    assert np.allclose(result[1], prediction.max())
