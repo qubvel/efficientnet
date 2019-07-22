1fff2787f532d70becc80d531ff6100d762a2cdb
qubvel <qubvel@gmail.com>
2019-07-23 00:13:55 +0300

    Update tests and .travis config

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,17 @@
 import os
 import sys
-sys.path.insert(0, '.')
 import pytest
-
 import numpy as np
-import efficientnet.keras as efn
 
 from skimage.io import imread
-from keras.models import load_model
+
+sys.path.insert(0, '.')
+if os.environ.get('TF_KERAS'):
+    import efficientnet.tfkeras as efn
+    from tensorflow.keras.models import load_model
+else:
+    import efficientnet.keras as efn
+    from keras.models import load_model
 
 PANDA_PATH = 'misc/panda.jpg'
 
@@ -62,6 +66,7 @@ def test_models_result(args):
     prediction = model.predict(input_)
     assert result[0] == prediction.argmax()
     assert np.allclose(result[1], prediction.max())
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
