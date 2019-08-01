37d3c43e7bd8c5db3f85a018bfdca7caa1d9155e
qubvel <qubvel@gmail.com>
2019-08-01 15:20:53 +0300

    Fix import error

--- efficientnet/tfkeras.py
+++ efficientnet/tfkeras.py
@@ -2,7 +2,6 @@ from . import inject_tfkeras_modules, init_tfkeras_custom_objects
 from . import model
 
 from .preprocessing import center_crop_and_resize
-from .preprocessing import preprocess_input as _preprocess_input
 
 EfficientNetB0 = inject_tfkeras_modules(model.EfficientNetB0)
 EfficientNetB1 = inject_tfkeras_modules(model.EfficientNetB1)
@@ -13,6 +12,6 @@ EfficientNetB5 = inject_tfkeras_modules(model.EfficientNetB5)
 EfficientNetB6 = inject_tfkeras_modules(model.EfficientNetB6)
 EfficientNetB7 = inject_tfkeras_modules(model.EfficientNetB7)
 
-preprocess_input = inject_keras_modules(_preprocess_input)
+preprocess_input = inject_keras_modules(model.preprocess_input)
 
 init_tfkeras_custom_objects()
