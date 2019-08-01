3646a41b3a4fc24a111627b0239fc79883056a5b
qubvel <qubvel@gmail.com>
2019-08-01 15:26:42 +0300

    Fix typo

--- efficientnet/tfkeras.py
+++ efficientnet/tfkeras.py
@@ -12,6 +12,6 @@ EfficientNetB5 = inject_tfkeras_modules(model.EfficientNetB5)
 EfficientNetB6 = inject_tfkeras_modules(model.EfficientNetB6)
 EfficientNetB7 = inject_tfkeras_modules(model.EfficientNetB7)
 
-preprocess_input = inject_keras_modules(model.preprocess_input)
+preprocess_input = inject_tfkeras_modules(model.preprocess_input)
 
 init_tfkeras_custom_objects()
