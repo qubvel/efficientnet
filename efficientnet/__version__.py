d647d0de79a9e53ee87f5ef3556ecd1556e8be2e
qubvel <qubvel@gmail.com>
2019-08-08 18:15:09 +0300

    Change version

--- efficientnet/__version__.py
+++ efficientnet/__version__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-VERSION = (1, 0, '0b2')
+VERSION = (1, 0, '0b3')
 
 __version__ = ".".join(map(str, VERSION))
 
