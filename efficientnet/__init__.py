9e4c560bd06da3fd90c6aeee51b48da22f2318b7
Simon Larsson <larssonsimon0@gmail.com>
2019-09-06 08:21:20 +0200

    Added missing __version__ import (#67)

--- efficientnet/__init__.py
+++ efficientnet/__init__.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 
 import functools
+from .__version__ import __version__
 
 _KERAS_BACKEND = None
 _KERAS_LAYERS = None
