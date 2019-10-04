02c565c3d6292d90dc18560c4f9bd1a2e6a9f8d4
Pavel Yakubovskiy <qubvel@gmail.com>
2019-10-04 12:29:57 +0300

    Update __version__.py

--- efficientnet/__version__.py
+++ efficientnet/__version__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-VERSION = (1, 0, '0b3')
+VERSION = (1, 0, 0)
 
 __version__ = ".".join(map(str, VERSION))
 
